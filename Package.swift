49ce1365bc6b4943a1720ab4feb5c22eb5281d79
ykyivskyi-gl <yevhen.kyivskyi@glia.com>
2025-09-17 12:02:02 +0200

    feat: Setup SPM

--- Package.swift
+++ Package.swift
@@ -14,28 +14,14 @@ let package = Package(
     .library(
       name: "OpenTelemetryProtocolExporterHTTP", targets: ["OpenTelemetryProtocolExporterHttp"]
     ),
-    .library(name: "OTelSwiftLog", targets: ["OTelSwiftLog"]),
-    .library(name: "OpenTelemetryApi", targets: ["OpenTelemetryApi"]),
-    .library(name: "OpenTelemetrySdk", targets: ["OpenTelemetrySdk"]),
-    .library(name: "StdoutExporter", targets: ["StdoutExporter"]),
+    .library(name: "ResourceExtension", targets: ["ResourceExtension"]),
   ],
   dependencies: [
     .package(url: "https://github.com/open-telemetry/opentelemetry-swift-core.git", from: "2.1.0"),
     .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.29.0"),
     .package(url: "https://github.com/apple/swift-log.git", from: "1.6.3"),
-    .package(url: "https://github.com/apple/swift-metrics.git", from: "2.7.0"),
-    .package(url: "https://github.com/mw99/DataCompression", from: "3.9.0")
   ],
   targets: [
-    .target(
-      name: "OTelSwiftLog",
-      dependencies: [
-        .product(name: "OpenTelemetryApi", package: "opentelemetry-swift-core"),
-        .product(name: "Logging", package: "swift-log")
-      ],
-      path: "Sources/Bridges/OTelSwiftLog",
-      exclude: ["README.md"]
-    ),
     .target(
       name: "OpenTelemetryProtocolExporterCommon",
       dependencies: [
@@ -50,86 +36,15 @@ let package = Package(
       dependencies: [
         .product(name: "OpenTelemetrySdk", package: "opentelemetry-swift-core"),
         "OpenTelemetryProtocolExporterCommon",
-        .product(
-          name: "DataCompression",
-          package: "DataCompression",
-          condition: .when(platforms: [.macOS, .iOS, .watchOS, .tvOS, .visionOS])
-        ),
       ],
       path: "Sources/Exporters/OpenTelemetryProtocolHttp"
     ),
     .target(
-        name: "OpenTelemetryApi",
+        name: "ResourceExtension",
         dependencies: [
-            .product(name: "OpenTelemetryApi", package: "opentelemetry-swift-core")
-        ]
-    ),
-    .target(
-        name: "OpenTelemetrySdk",
-        dependencies: [
-            .product(name: "OpenTelemetrySdk", package: "opentelemetry-swift-core"),
-            "OpenTelemetryApi"
-        ]
-    ),
-    .target(
-        name: "StdoutExporter",
-        dependencies: [
-            .product(name: "StdoutExporter", package: "opentelemetry-swift-core"),
-            "OpenTelemetrySdk"
-        ]
+          .product(name: "OpenTelemetrySdk", package: "opentelemetry-swift-core")
+        ],
+        path: "Sources/Instrumentation/SDKResourceExtension",          exclude: ["README.md"]
     ),
   ]
-).addPlatformSpecific()
-
-extension Package {
-  func addPlatformSpecific() -> Self {
-
-    #if canImport(Darwin)
-      products.append(contentsOf: [
-        .library(name: "NetworkStatus", targets: ["NetworkStatus"]),
-        .library(name: "URLSessionInstrumentation", targets: ["URLSessionInstrumentation"]),
-        .library(name: "ResourceExtension", targets: ["ResourceExtension"]),
-      ])
-      targets.append(contentsOf: [
-        .target(
-          name: "NetworkStatus",
-          dependencies: [
-            .product(name: "OpenTelemetryApi", package: "opentelemetry-swift-core")
-          ],
-          path: "Sources/Instrumentation/NetworkStatus",
-          linkerSettings: [.linkedFramework("CoreTelephony", .when(platforms: [.iOS]))]
-        ),
-        .target(
-          name: "URLSessionInstrumentation",
-          dependencies: [
-            .product(name: "OpenTelemetrySdk", package: "opentelemetry-swift-core"),
-            "NetworkStatus"],
-          path: "Sources/Instrumentation/URLSession",
-          exclude: ["README.md"]
-        ),
-        .target(
-          name: "ResourceExtension",
-          dependencies: [
-            .product(name: "OpenTelemetrySdk", package: "opentelemetry-swift-core")
-          ],
-          path: "Sources/Instrumentation/SDKResourceExtension",
-          exclude: ["README.md"]
-        ),
-      ])
-    #endif
-
-    return self
-  }
-}
-
-if ProcessInfo.processInfo.environment["OTEL_ENABLE_SWIFTLINT"] != nil {
-  package.dependencies.append(contentsOf: [
-    .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", from: "0.57.1")
-  ])
-
-  for target in package.targets {
-    target.plugins = [
-      .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")
-    ]
-  }
-}
+)
